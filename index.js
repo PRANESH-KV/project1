b0ae5d210b4c16680a046ecb0c5add7ee224a1d5
PRANESH-KV <praneshkv2@gmail.com>
2026-03-11 22:57:20 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { NotificationProvider } from "./context/NotificationContext";
+import "./App.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <NotificationProvider>
+    <App />
+  </NotificationProvider>
+);
